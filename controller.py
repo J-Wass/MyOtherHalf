2187d3332c54c79c3a03f5078b50b6d7e7c0acc5
LeChosenOne <jonahwasserman97@gmail.com>
2017-08-08 21:26:34 -0400

    Update convention for Flask and added content to html game

--- controller.py
+++ controller.py
@@ -19,52 +19,54 @@ app.secret_key = key
 socketio = SocketIO(app)
 
 # list of Usernames in the game already
-Usernames = ['mod', 'Mod', 'moderator', 'Moderator', 'admin', 'Admin', 'administrator', 'Administrator', 'LeChosenOne']
-Rooms = []
+usernames = ['mod', 'Mod', 'moderator', 'Moderator', 'admin', 'Admin', 'administrator', 'Administrator', 'LeChosenOne']
+rooms = []
 #END GLOBALS
 
 #MODEL METHODS
-def CreateUser(nickname):
-    Usernames.append(nickname)
+def createUser(nickname):
+    usernames.append(nickname)
     return Player(nickname)
 
-def GetRoomById(id): #returns None if room can't be found
-    room = next((r for r in Rooms if r.roomId == id), None)#get the first room with the id, or none
+def getRoomById(id): #returns None if room can't be found
+    room = next((r for r in rooms if r.room_id == id), None)#get the first room with the id, or none
     return room
 
-def FindOrCreateRoom(user): #returns None if an error occurs in room organization
-    maxRoomId = 0
-    if Rooms:#if there are rooms, get the biggest one and find the id
-        maxRoomId = max(Room.roomId for Room in Rooms)
-        print maxRoomId
-        maxRoom = GetRoomById(maxRoomId)
-        if(maxRoom == None): #if room couldn't be found, but its id is in the room listing
+def addUserToFreeRoom(user): #returns None if an error occurs in room organization
+    max_room_id = 0
+    if rooms:#if there are rooms, get the biggest one and find the id
+        max_room_id = max(room.room_id for room in rooms)
+        max_room = getRoomById(max_room_id)
+        if(max_room == None): #if room couldn't be found, but its id is in the room listing
              return None
-        if maxRoom.playerCount < 2: #joining max room
-            if(maxRoom.AddPlayer(user) == None): #if room is full, but isn't supposed to be
+        if max_room.player_count < 2: #joining max room
+            if(max_room.addPlayer(user) == None): #if room is full, but isn't supposed to be
                 return None
-            Rooms.append(maxRoom)
-            return maxRoom
-    newRoom = Room(maxRoomId + 1)#create the new largest room
-    newRoom.AddPlayer(user)
-    Rooms.append(newRoom)
-    return newRoom
+            rooms.append(max_room)
+            return max_room
+    new_room = Room(max_room_id + 1)#create the new largest room
+    new_room.addPlayer(user)
+    rooms.append(new_room)
+    return new_room
 #END MODEL METHODS
 
 #ROUTING METHODS
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.j2')
+    #return render_template('index.j2')
+    user = createUser("LeChosenOne")
+    room = addUserToFreeRoom("LeChosenOne")
+    return render_template('game.j2', User = user, Room = room )
 
 
 @app.route('/game', methods=['POST'])
 def game():
     nickname = str(request.form['nickname'])
-    if nickname in Usernames:
+    if nickname in usernames:
         return render_template('index.j2', error = "Nickname: '" + nickname + "' is already in use!")
-    user = CreateUser(nickname)
-    room = FindOrCreateRoom(user)
+    user = createUser(nickname)
+    room = addUserToFreeRoom(user)
     if(user == None or room == None):
         return render_template('index.j2', error = "There was an error finding a room. Please try again later.")
     return render_template('game.j2', User = user, Room = room )
@@ -73,9 +75,9 @@ def game():
 #HANDLE SOCKET TRAFFIC
 @socketio.on('joinroom')
 def on_joinroom(data):
-    roomid = data['roomid']
+    room_id = data['room_id']
     username = str(data['username'])
-    join_room(roomid)
-    print('player joined room: ' + str(username) + ' ' + str(roomid))
-    emit('joinroom response', str(username) + ' ' + str(roomid), room=roomid)
+    join_room(room_id)
+    print('player joined room: ' + str(username) + ' ' + str(room_id))
+    emit('joinroom response', str(username) + ' ' + str(room_id), room=room_id)
 #END SOCKET TRAFFIC
--- models/Room.py
+++ models/Room.py
@@ -1,21 +1,21 @@
 from Player import Player
 
 class Room:
-    roomId = -1
+    room_id = -1
     player1 = Player('null')
     player2 = Player('null')
     level = -1
-    playerCount = 0
+    player_count = 0
     def __init__(self, id):
-        self.roomId = id
+        self.room_id = id
     #adds a new player to the room, returns None if the room is full
-    def AddPlayer(self, player):
-        if self.playerCount == 0:
+    def addPlayer(self, player):
+        if self.player_count == 0:
             self.player1 = player
-            self.playerCount += 1
-        elif self.playerCount == 1:
+            self.player_count += 1
+        elif self.player_count == 1:
             self.player2 = player
-            self.playerCount += 1
+            self.player_count += 1
         else:
             return None
         return player
